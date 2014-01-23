68ecfc46068830b273562a55e6947bca0f9c0070
Quentin <Quentin@POSTE017>
2014-01-23 11:49:33 +0100

    FIX : Migration from CDO => modify column cdo_resource to NULLABLE

--- org.eclipse.emf.db/src/org/eclipse/emf/db/util/DBUtil.java
+++ org.eclipse.emf.db/src/org/eclipse/emf/db/util/DBUtil.java
@@ -286,6 +286,7 @@ public final class DBUtil {
             for (EClass clazz : concreteClasses) {
                 stmt.execute("ALTER TABLE " + DBQueryUtil.getTableName(clazz) + " CHANGE COLUMN `cdo_id` `cdo_id` BIGINT(20) NOT NULL AUTO_INCREMENT ;");
                 stmt.execute("ALTER TABLE " + DBQueryUtil.getTableName(clazz) + " MODIFY `cdo_container` BIGINT(20) ;");
+                stmt.execute("ALTER TABLE " + DBQueryUtil.getTableName(clazz) + " MODIFY `cdo_resource` BIGINT(20) ;");
                 for (EReference ref : clazz.getEAllReferences()) {
                     if (DBModelInformationCache.hasInheritance(ref)) {
                         for (EClass clazz2 : concreteClasses) {
